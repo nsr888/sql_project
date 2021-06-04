895cb4c9f5f0ff1fdb4d2b73cbe7f496ed338eec
Artur Nasyrov <anasyrov@mail.ru>
2021-06-04 22:46:47 +0300

    Append archive dir creation

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 rm sber.db
 unzip data.zip '*01032021*'
 python3 main.py
+mkdir -p archive
 for FILENAME in *.xlsx; do mv $FILENAME ./archive/$FILENAME.backup; done
-for FILENAME in *.txt; do mv $FILENAME ./archive/$FILENAME.backup; done
+for FILENAME in transactions*.txt; do mv $FILENAME ./archive/$FILENAME.backup; done
